81f4f8d48a16c7e535a0924e2d0d827b846f9d0c
AdarshThakur398 <nutius90@gmail.com>
2025-02-07 23:30:40 +0530

    creating embedding snippets using ifram and some upload handling

--- backend/backend.js
+++ backend/backend.js
@@ -5,6 +5,7 @@ const cors=require('cors');
 const path=require('path');
 const app=express();
 app.use(cors());
+app.use(express.json());
 app.use('/uploads',express.static('uploads'));
 
 
@@ -29,11 +30,46 @@ const upload = multer({
     }
 })
 
-const validateVideoUrl=(url) => {
-    const platforms =[
-        {
-            name:'youtube',
-            patters
-        }
-    ]
+app.post('/api/upload',upload.single('video'),(req,res) => {
+    if(!req.file) {
+        return res.status(400).json({error:'NO file Uploaded'});
+
+    }
+    res.json({
+        videoUrl:`/uploads/${req.file.filename}`
+    });
+})
+app.post('/api/generated-embed',(req,res)=> {
+    const {videoUrl,platform}=req.body;
+    const embedTemplate = {
+        youtube: (id) => `
+      <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/${id}" 
+        frameborder="0" 
+        allowfullscreen>
+      </iframe>
+    `,
+    dailymotion: (id) => `
+    <iframe 
+      width="560" 
+      height="315" 
+      src="https://www.dailymotion.com/embed/video/${id}" 
+      frameborder="0" 
+      allowfullscreen>
+    </iframe>
+  `,
+  local: (url) => `
+  <video width="560" height="315" controls>
+    <source src="${url}" type="video/mp4">
+    Your browser does not support the video tag.
+  </video>
+`
+};
+try {
+    
 }
+    }
+})
+
